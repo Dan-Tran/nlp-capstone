dca55d97fa8bc952637c748dafafb80447c56d45
Dan Tran <dantran@dantran.tech>
2019-05-28 14:46:39 -0700

    Fix bugs with testing

---
+++ blogcode/blog10/test.sh
@@ -0,0 +1,1 @@
+allennlp evaluate --output-file out.json --cuda-device 0 --include-package model --include-package test_reader ser/model.tar.gz /projects/instr/19sp/cse481n/DJ2/nlvr/nlvr2/data/test1.json
